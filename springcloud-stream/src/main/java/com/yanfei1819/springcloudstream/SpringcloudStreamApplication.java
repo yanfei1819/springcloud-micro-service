11743ffdac8ecf37eecb8858a02e91252d0d0b6c
yanfei1819 <yanfei1819@163.com>
2019-01-11 15:39:34 +0800

    消息驱动环境搭建（未完待续）

---
+++ springcloud-stream/src/main/java/com/yanfei1819/springcloudstream/SpringcloudStreamApplication.java
@@ -0,0 +1,14 @@
+package com.yanfei1819.springcloudstream;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringcloudStreamApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringcloudStreamApplication.class, args);
+	}
+
+}
+
